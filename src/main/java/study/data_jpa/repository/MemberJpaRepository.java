d72198382e4584c2290e023669bd89d3070df5e5
sooin <lilesooin@gmail.com>
2025-10-22 17:01:06 -0400

    순수 JPA 페이징과 정렬2

--- src/main/java/study/data_jpa/repository/MemberJpaRepository.java
+++ src/main/java/study/data_jpa/repository/MemberJpaRepository.java
@@ -68,6 +68,6 @@ public class MemberJpaRepository {
     public long totalCount(int age) {
         return em.createQuery("select count(m) from Member m where m.age = :age", Long.class)
                 .setParameter("age", age)
-                .getFirstResult();
+                .getSingleResult();
     }
 }
