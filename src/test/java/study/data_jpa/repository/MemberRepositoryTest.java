294ae2e6a2ee43a993e29bf3e16f346fd5ff4273
sooin <lilesooin@gmail.com>
2025-10-23 18:32:45 -0400

    엔티티 그래프

--- src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
@@ -221,4 +221,34 @@ class MemberRepositoryTest {
         // then
         assertThat(resultCount).isEqualTo(3);
     }
+
+    @Test
+    public void findMemberLazy() {
+        // given
+        // member1 -> teamA
+        // member2 -> teamB
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        teamRepository.save(teamA);
+        teamRepository.save(teamB);
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 10, teamB);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        em.flush();
+        em.clear();
+
+        // when N(10) + 1 문제 -> 패치조인으로 해결
+        // select Member 1
+        // List<Member> members = memberRepository.findAll(); // proxy 객체(Team)
+        List<Member> members = memberRepository.findMemberFetchJoin(); // join쿼리로 team객체도 세팅함
+
+        for (Member member : members) {
+            System.out.println("member = " + member.getUsername());
+            System.out.println("member.teamClass = " + member.getTeam().getClass());
+            System.out.println("member.team = " + member.getTeam().getName());
+        }
+    }
 }
