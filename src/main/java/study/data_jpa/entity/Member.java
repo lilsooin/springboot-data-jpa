76c2f9d68f01610797340d6fad021cb9c724a98f
sooin <lilesooin@gmail.com>
2025-10-24 18:10:57 -0400

    Auditing

--- src/main/java/study/data_jpa/entity/Member.java
+++ src/main/java/study/data_jpa/entity/Member.java
@@ -2,6 +2,7 @@ package study.data_jpa.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 @Entity
 @Getter @Setter
@@ -11,7 +12,7 @@ import lombok.*;
         name = "Member.findByUsername",
         query = "select m from Member m where m.username = :username"
 )
-public class Member {
+public class Member extends BaseEntity {
 
     @Id @GeneratedValue
     @Column(name = "member_id")
