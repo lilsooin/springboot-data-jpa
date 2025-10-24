4abc5010133428736b99e4250d5910771a6d1790
sooin <lilesooin@gmail.com>
2025-10-24 17:18:24 -0400

    사용자 정의 리포지토리 구현

--- src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
@@ -279,4 +279,9 @@ class MemberRepositoryTest {
         List<Member> findMember = memberRepository.findLockByUsername("member1");
 
     }
+
+    @Test
+    public void callCustom() {
+        List<Member> result = memberRepository.findMemberCustom();
+    }
 }
