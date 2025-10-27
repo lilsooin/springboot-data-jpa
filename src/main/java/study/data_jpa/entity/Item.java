234e5f7525bad03a5f15ca1c77e6e32b1a36f7cb
sooin <lilesooin@gmail.com>
2025-10-26 23:18:24 -0400

    Specifications (명세)

--- src/main/java/study/data_jpa/entity/Item.java
+++ src/main/java/study/data_jpa/entity/Item.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Id;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.domain.Persistable;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.time.LocalDateTime;
 
