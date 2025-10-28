2ada0daaacbbac6df92e674bfbc8d863cf330214
sooin <lilesooin@gmail.com>
2025-10-28 17:58:35 -0400

    Projections

--- src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
@@ -337,5 +337,27 @@ class MemberRepositoryTest {
         assertThat(result.get(0).getUsername()).isEqualTo("m1");
 
     }
+
+    @Test
+    public void projections() {
+        // given
+        Team teamA = new Team("TeamA");
+        em.persist(teamA);
+
+        Member m1 = new Member("m1", 0, teamA);
+        Member m2 = new Member("m2", 0, teamA);
+        em.persist(m1);
+        em.persist(m2);
+
+        em.flush();
+        em.clear();
+
+        // when
+        List<NestedClosedProjections> result = memberRepository.findProjectionsByUsername("m1", NestedClosedProjections.class);
+
+        for (NestedClosedProjections usernameOnly : result) {
+            System.out.println("usernameOnly = " + usernameOnly);
+        }
+    }
 }
 
